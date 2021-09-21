f82d532d929a2c52ece285aa0ad4c93851dd3fbb
johnsonjw48 <73818994+johnsonjw48@users.noreply.github.com>
2021-09-21 12:18:10 +0200

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -39,7 +39,7 @@ class About extends Component {
                </div>
                <div className="columns download">
                   <p>
-                     <a href={resumeDownload} className="button" target="_blank"><i className="fa fa-download"></i>Téléchager mon CV</a>
+                     <a href={resumeDownload} download className="button" target="_blank"><i className="fa fa-download"></i>Téléchager mon CV</a>
                   </p>
                </div>
             </div>
